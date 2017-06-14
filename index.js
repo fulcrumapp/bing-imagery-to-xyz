a1fa2f324f2ec78f89b996da9588df146354469d
Tyler Johnston <tylerjohnst@gmail.com>
2017-06-14 16:15:07 -0400

    Add root url for help and multiple tile types

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+var README = fs.readFileSync('index.txt').toString();
 
 function toQuadKey(x, y, z) {
   var index = ''
@@ -17,8 +20,24 @@ function toQuadKey(x, y, z) {
   return index
 }
 
-app.get('/:z/:x/:y', function(req, res) {
-  res.redirect(`https://ecn.t3.tiles.virtualearth.net/tiles/a${toQuadKey(req.params.x, req.params.y, req.params.z)}.jpeg?g=587&mkt=en-gb&n=z`)
+function source(source) {
+  return SOURCES[source] || SOURCES['a'];
+}
+
+var SOURCES = {
+  'aerial': 'a',
+  'streets': 'r'
+}
+
+app.get('/', function(req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send(README);
+});
+
+app.get('/:source/:z/:x/:y', function(req, res) {
+  var prefix = source(req.params.source);
+  var quadKey = toQuadKey(req.params.x, req.params.y, req.params.z);
+  res.redirect(`https://ecn.t3.tiles.virtualearth.net/tiles/${prefix}${quadKey}.jpeg?g=587&mkt=en-gb&n=z`);
 });
 
 app.listen(app.get('port'), function() {
